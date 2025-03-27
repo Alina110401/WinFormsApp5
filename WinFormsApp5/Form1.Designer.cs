5466baa533d4cfa3c9e0e75b63b232fed89013ef
Alina110401 <slabko.lina01@gmail.com>
2025-03-27 12:50:34 +0100

    czerwona

--- WinFormsApp5/Form1.Designer.cs
+++ WinFormsApp5/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             pictureBox1 = new PictureBox();
             button1 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -53,11 +57,63 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(157, 49);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(62, 29);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "90 ";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.Click += button90_Click;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(157, 94);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(67, 29);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "180";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            radioButton2.Click += button180_Click;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(157, 139);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(67, 29);
+            radioButton3.TabIndex = 4;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "270";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            radioButton3.Click += button270_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(121, 194);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 5;
+            button2.Text = "Rotate";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button_rotate_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
             Controls.Add(button1);
             Controls.Add(pictureBox1);
             Name = "Form1";
@@ -71,5 +127,9 @@
 
         private PictureBox pictureBox1;
         private Button button1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Button button2;
     }
 }
--- WinFormsApp5/Form1.cs
+++ WinFormsApp5/Form1.cs
@@ -12,13 +12,69 @@ namespace WinFormsApp5
             string sciezkaObrazu = @"C:\Users\User\Desktop\wizual\Obrazek_Piesek.jpg";
 
             pictureBox1.Image = Image.FromFile(sciezkaObrazu);
-pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
 
         }
 
         private void button_image_Click(object sender, EventArgs e)
         {
- 
+
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button90_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button180_Click(object sender, EventArgs e)
+        {
+       
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_rotate_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                // zmiana 
+                pictureBox1.Refresh();
+            }
+
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                // zmiana 
+                pictureBox1.Refresh();
+            }
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                // zmiana 
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void button270_Click(object sender, EventArgs e)
+        {
+           
+
 
         }
     }
